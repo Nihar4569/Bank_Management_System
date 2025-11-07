1356e100cbbd1691169802957afe316e19aeb152
Nihar4569 <nihar4569@gmail.com>
2025-11-07 15:32:52 +0530

    Added some other test case

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,17 +1,22 @@
-# tests/test_scraper.py
-from app.scraper import scrape_interest_rates, scrape_bank_names
+import requests
 
-def test_scrape_interest_rates():
-    """Ensure scraper returns structured bank data."""
-    data = scrape_interest_rates()
+BASE_URL = "http://127.0.0.1:5000"
+
+def test_interest_rates():
+    res = requests.get(f"{BASE_URL}/scrape/interest-rates")
+    assert res.status_code == 200
+    data = res.json()
+    print("Interest Rates Data:", data[:2])
     assert isinstance(data, list)
     assert len(data) > 0
-    assert all("bank" in d for d in data)
-    assert all("info" in d for d in data)
 
-
-def test_scrape_bank_names():
-    """Ensure scraper fetches some bank names."""
-    data = scrape_bank_names()
+def test_bank_names():
+    res = requests.get(f"{BASE_URL}/scrape/bank-names")
+    assert res.status_code == 200
+    data = res.json()["banks"]
+    print("Banks:", data)
     assert isinstance(data, list)
-    assert all(isinstance(name, str) for name in data)
+    assert len(data) > 0
+
+test_interest_rates()
+test_bank_names()
